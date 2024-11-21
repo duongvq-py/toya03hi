4e9ade561ee57a9c2c8dac93152119c89281fb84
duongvq-py <duongvq@gmail.com>
2024-11-21 09:50:15 +0000

    Bai 2

--- main.py
+++ main.py
@@ -2,21 +2,57 @@
 from s00_bailam import hi as bailam_f
 #endregion import bailam_f
 
-
 #region chambai
 from s02_chambai import chambai
 
 #region testkey_list
 testcase_list = [
-  {'tc_name': 'tc0', 'input': {'name':'Mom'}, 'output':'Hi Mom!'},
-  {'tc_name': 'tc3', 'input': ['Mom'],        'output':'Hi Mom!'},
-  {'tc_name': 'tc4', 'input': {'name':''},    'output':'Hi!'},
-  {'tc_name': 'tc1', 'input': {},             'output':'Hi!'},
-  {'tc_name': 'tc2', 'input': {'name':None},  'output':'Hi!'},
-
-  {'tc_name': 'tckho1', 'input': ['Mom', 'Dad'],             'output':'Hi Mom, and Dad!'},
-  {'tc_name': 'tckho2', 'input': ['A', 'B', 'C'],            'output':'Hi A, B, and C!'},
-  {'tc_name': 'tckho3', 'input': ['1', '22', '333', '4444'], 'output':'Hi 1, 22, 333, and 4444!'},
+    {
+        'tc_name': 'tc0',
+        'input': {
+            'name': 'Mom'
+        },
+        'output': 'Hi Mom!'
+    },
+    {
+        'tc_name': 'tc3',
+        'input': ['Mom'],
+        'output': 'Hi Mom!'
+    },
+    {
+        'tc_name': 'tc4',
+        'input': {
+            'name': ''
+        },
+        'output': 'Hi!'
+    },
+    {
+        'tc_name': 'tc1',
+        'input': {},
+        'output': 'Hi!'
+    },
+    {
+        'tc_name': 'tc2',
+        'input': {
+            'name': None
+        },
+        'output': 'Hi!'
+    },
+    {
+        'tc_name': 'tckho1',
+        'input': ['Mom', 'Dad'],
+        'output': 'Hi Mom, and Dad!'
+    },
+    {
+        'tc_name': 'tckho2',
+        'input': ['A', 'B', 'C'],
+        'output': 'Hi A, B, and C!'
+    },
+    {
+        'tc_name': 'tckho3',
+        'input': ['1', '22', '333', '4444'],
+        'output': 'Hi 1, 22, 333, and 4444!'
+    },
 ]
 #endregion testkey_list
 
@@ -24,12 +60,12 @@ ketqua_list = []
 for tc in testcase_list:  # tc aka testcase
   INP_name = tc['input']
   tc_score, o_BAILAM = chambai(tc, bailam_f)
-  
+
   ketqua_list.append({
-    'tc_name'    : tc['tc_name'],
-    'tc_score'   : tc_score,  
-    'o_TESTCASE' : tc['output'],    
-    'o_BAILAM'   : o_BAILAM,  
+      'tc_name': tc['tc_name'],
+      'tc_score': tc_score,
+      'o_TESTCASE': tc['output'],
+      'o_BAILAM': o_BAILAM,
   })
 #endregion chambai
 
@@ -40,9 +76,10 @@ for kq in ketqua_list:
 {kq['tc_name']} {kq['tc_score']}
 o_TESTCASE = {kq['o_TESTCASE']}
 o_BAILAM   = {kq['o_BAILAM']}
-  '''.strip()+'\n')
+  '''.strip() + '\n')
 
 print('\n---ketqua')
+
 for kq in ketqua_list:
   print(f'''{kq['tc_name']} {kq['tc_score']}''')
 #endregion in ketqua
--- s00_bailam.py
+++ s00_bailam.py
@@ -27,9 +27,27 @@ hi('Mom', 'Dad')             | Hi Mom, and Dad!
 hi('A', 'B', 'C')            | Hi A, B, and C!
 hi('1', '22', '333', '4444') | Hi 1, 22, 333, and 4444!
 """
+
 #endregion debai
 
+
 #region bailam
-def hi(*args):
-   return 'TODO'
+def hi(*args, name=None):
+   if not args and not name:
+      # Neu tc co name la null hoac ko ton tai thi return hi!
+      return "Hi!"
+   if name:
+      return f"Hi {name}!"
+   # neu tc co name not null va input 1 value thi return ve hi name
+   args = [str(arg) for arg in args if arg]
+   # Vong lap cho nhung tc co name not null va input hon 1 gia tri
+   if not args:
+      return "Hi!"
+   if len(args) == 1:
+      return f"Hi {args[0]}!"
+   if len(args) == 2:
+      return f"Hi {args[0]}, and {args[1]}!"
+   return f"Hi {', '.join(args[:-1])}, and {args[-1]}!"
+
+
 #endregion bailam
